01cc49d07568cf74b72b64227db8acfe9c6f4dc7
Martyn Whitwell <martyn.whitwell@gmail.com>
2016-03-11 19:09:15 +0000

    Now authenticating with Openbooks via oauth2!

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "Flask-Migrate==1.6.0",
         "sqlalchemy-utils==0.31.4",
         "Flask-OAuthlib==0.9.2",
+        "oauth2client==2.0.1",
         # for deployment
         "gunicorn",
         "newrelic",
